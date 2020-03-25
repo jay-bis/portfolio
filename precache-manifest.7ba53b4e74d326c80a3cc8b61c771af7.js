62cae938bc2cd3bd41bd0e704ff1845b3b1ccb39
Jack Biscupski <jbiscupski@uiowa.edu>
2020-03-25 18:01:40 -0500

    Updates

---
+++ precache-manifest.7ba53b4e74d326c80a3cc8b61c771af7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0aa9bc5844fe8952c1b102ccbb3dc09",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "295f2a5167e1cc823ac7",
+    "url": "/portfolio/static/css/main.5f45d2d1.chunk.css"
+  },
+  {
+    "revision": "7a48ac52de4d4e3029b6",
+    "url": "/portfolio/static/js/2.480d8750.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.480d8750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295f2a5167e1cc823ac7",
+    "url": "/portfolio/static/js/main.0b3d17a3.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "7538fd458720425185f4a17ea5353721",
+    "url": "/portfolio/static/media/DEM.7538fd45.png"
+  },
+  {
+    "revision": "9abe13cbf8e06359973e03ea8d5f835d",
+    "url": "/portfolio/static/media/chicago.9abe13cb.jpg"
+  },
+  {
+    "revision": "0578e14b6e11278629dc557e5b42b72a",
+    "url": "/portfolio/static/media/code.0578e14b.jpg"
+  },
+  {
+    "revision": "47f483ab45582812d0e402fefdc288ae",
+    "url": "/portfolio/static/media/flightdropper.47f483ab.png"
+  },
+  {
+    "revision": "17d502e97135365e46869a02223ad77b",
+    "url": "/portfolio/static/media/mountains.17d502e9.jpg"
+  },
+  {
+    "revision": "629ccf9e33b19612bf249f6ebd5dc48a",
+    "url": "/portfolio/static/media/plotly.629ccf9e.png"
+  },
+  {
+    "revision": "229e8f330c8279b2e9468716685ed38e",
+    "url": "/portfolio/static/media/scatter.229e8f33.png"
+  }
+]);
